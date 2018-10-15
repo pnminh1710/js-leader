3f3252eea8c7c94e881b8ce12c78c5daa1833678
Albus Phan <pnminh1710@gmail.com>
2018-10-15 10:08:28 +0700

    Do exercice 3 in lesson 5

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// This is main file js for coding.
+/**
+ * Sử dụng kiến thức đã học khai báo một vài object khác nhau mà bạn có thể tưởng tượng ra. Ví dụ:
+ *  var myCommunity = {
+ *    name: 'Coders.Tokyo',
+ *    numberOfMembers: 10000,
+ *    isAwesome: true
+ *  };
+ */
+
+const myLove = {
+  name: 'Tu Zanh Le',
+  age: 23,
+  location: 'Phan Thiet',
+  isBeautiful: true,
+};
