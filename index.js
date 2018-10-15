aa73aee75fe56c1e65dfb8069d0fef837ffc6fcc
Albus Phan <pnminh1710@gmail.com>
2018-10-15 13:44:19 +0700

    Do exercise in lesson 9

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var a = 10;
 
 /**
- * Viết ra tất cả các cách khác nhau có thể (trong phạm vi bài học) để tăng a lên 2 đơn vị
+ * Viết ra tất cả các cách khác nhau có thể (trong phạm vi bài học) để gấp đôi a
  */
 
-// a = a + 2
-// a += 2
-// b = 2; a = a + b;
+// a = a * 2;
+// a *= 2;
