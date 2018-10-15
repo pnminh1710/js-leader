2c2e910d845230ecbaa6b0ffce446d771ae87252
Albus Phan <pnminh1710@gmail.com>
2018-10-15 10:01:15 +0700

    Do exercice 2 in lesson 4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// This is main file js for coding.
+/**
+ * Sử dụng các kiến thức đã học trong bài khai báo các biến mô tả một người, vật nào đó mà bạn thích
+ * Ví dụ dưới đây mô tả thời tiết ngày hôm nay
+ *  var today = 'Sunday';
+ *  var isCloudy = true;
+ *  var currentTemperature = 30;
+ */
+
+const name = 'Albus Phan';
+const age = 23;
+const hobby = 'coding';
+const job = 'Front - end Developer';
