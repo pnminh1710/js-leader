f3d39d755c075fb521b4bebcabf3cc3376e5504a
Albus Phan <pnminh1710@gmail.com>
2018-10-15 11:33:41 +0700

    Do exercise 6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// This is main file js for coding.
+var a = 10;
+
+var x = --a + a++;
+console.log(x);
+
+// Nhấn Run để chạy code.
+// Giải thích vì sao kết quả là 18.
+
+// a = 10;
+// x = --a + a++;
+// x = 9 + a++; a = 9
+// x = 9 + 9; x = 18 a = 10 final;
