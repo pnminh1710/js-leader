316e989251001d7ba177870905c729b71e26c10c
Albus Phan <pnminh1710@gmail.com>
2018-10-15 13:53:46 +0700

    Do exercise in lesson 10

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// This is main file js for coding.
+/**
+ * Khai báo biến `rectangle` là 1 object gồm có các property:
+ * - width: chiều rộng, là 1 số bất kì
+ * - height: chiều cao, là 1 số bất kì
+ * Viết các method:
+ * - getWidth: trả về chiều rộng
+ * - getHeight: trả về chiều cao
+ * - getArea: trả về diện tích
+ */
+
+const rectangle = {
+  width: 20,
+  height: 10,
+  getWidth: function() {
+    return this.width;
+  },
+  getHeight: function() {
+    return this.height;
+  },
+  getArea: function() {
+    return this.width * this.height;
+  },
+};
