ecbdc034a543eb187de408f9b862e12656a42305
Albus Phan <pnminh1710@gmail.com>
2018-10-15 10:16:56 +0700

    Do exercice 4 in lesson 6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// This is main file js for coding.
+/**
+ * Cách sử dụng Array và Object khác nhau như thế nào. Viết câu trả lời ở phía dưới.
+ */
+
+
+// Array đai điện cho mỗi tập hợp các giá trị cùng kiển với nhau, có sự tương đồng về cấu trúc.
+// Object thể hiện tính chất của một vật (things) và các thuộc tính của nó. 
