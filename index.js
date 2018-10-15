a1cf90a935aefebe29fdecfbb42fd8aaa7082106
Albus Phan <pnminh1710@gmail.com>
2018-10-15 14:03:15 +0700

    Do exercise in lesson 12

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // This is main file js for coding.
+
+var a = {
+  foo: 'bar'
+};
+
+var b = {
+  foo: 'bar'
+};
+
+console.log(a === b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù giá trị của 2 object có vẻ giống nhau. Viết câu trả lời ở dưới đây.
+
+
+// Two objects are strictly equal if they refer to the same Object.
+// Trong truong hop nay 2 object dang khong refer den 1 object nao do nen chung khong bang nhau
