d668c1f70d68a6ee0a466709abf465a93f5f9c35
Albus Phan <pnminh1710@gmail.com>
2018-10-15 14:06:16 +0700

    Do exercise

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var a = '1000';
-var b = '200';
+var a = [1, 2];
+var b = [1, 2];
+console.log(a === b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù giá trị của 2 array có vẻ giống nhau. Viết câu trả lời ở dưới đây.
 
-console.log(a > b);
-// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù 1000 trông có vẻ lớn hơn 200. Viết câu trả lời ở dưới đây.
-
-// Dang so sanh 2 chuoi chu ko phai so, suy ra ki tu dau tien '2' se lon hon '1'
+// Tuong tu nhu voi object vi ban chat cua array cung la object.
