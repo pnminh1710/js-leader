205f90c325bbf5319bdb40e269bd358445c51f17
Albus Phan <pnminh1710@gmail.com>
2018-10-15 09:49:26 +0700

    Do exercice 1 in lesson 3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// This is main file js for coding.
+// Bài tập
+// Khai báo 2 biến x, y nhận giá trị số bất kì
+// Khai báo các biến khác và lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.
+
+const x = 7;
+const y = 5;
+
+const add = x + y;
+const sub = x - y;
+const mul = x * y;
+const div = x / y;
