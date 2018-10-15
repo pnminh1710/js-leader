e452040647d81315b2db428b64d964aeaad76d6d
Albus Phan <pnminh1710@gmail.com>
2018-10-15 11:38:08 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var a = 10;
+var b = 18;
 
-var x = --a + a++;
+var x = a-- + b++ - ++b - ++a;
 console.log(x);
+// Nhấn Run để chạy code
+// Thêm cách dấu cách vào dòng 4 để code dễ đọc hơn
+// Giải thích tại sao kết quả lại như vậy. Gợi ý: Viết ra từng dòng để giải thích
 
-// Nhấn Run để chạy code.
-// Giải thích vì sao kết quả là 18.
-
-// a = 10;
-// x = --a + a++;
-// x = 9 + a++; a = 9
-// x = 9 + 9; x = 18 a = 10 final;
+// 10 + 18 - 20 - 10
+// x = -2
