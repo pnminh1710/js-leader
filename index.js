200eeb7f7397a086e0e117d589c80e60d6ff949a
Albus Phan <pnminh1710@gmail.com>
2018-10-15 13:42:23 +0700

    Do exercise in lesson 9

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// This is main file js for coding.
+var a = 10;
+
+/**
+ * Viết ra tất cả các cách khác nhau có thể (trong phạm vi bài học) để tăng a lên 2 đơn vị
+ */
+
+// a = a + 2
+// a += 2
+// b = 2; a = a + b;
