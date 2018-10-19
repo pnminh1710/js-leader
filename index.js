d7a27e3b1978ae780c4f63937688360ba6599557
Albus Phan <pnminh1710@gmail.com>
2018-10-19 20:55:06 +0700

    Do exercise

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-/**
- * Viết function trả về tích các số từ start đến end, không tính end 
- * Ví dụ: console.log(calculate(2, 5)) sẽ hiển thị ra màn hình 24 (vì 2 * 3 * 4 = 24)
- */
-function calculate(start, end) {
-  let total = 1;
-  for (let i = start; i < end; i++) {
-    total = total * i;
+function has(array, value) {
+  // sử dụng vòng lặp for...of viết hàm has để kiểm tra xem array có tồn tại 1 giá trị xác định hay không
+  // Tham số:
+  //	- array: mảng cần kiểm tra
+  //	- value: giá trị cần kiểm tra xem có tồn tại trong array không
+  // Return:
+  //	- true nếu có tồn tại
+  //	- false nếu không tồn tại
+  for (const data of array) {
+    if (data === value) {
+      return true;
+    }
+    return false;
   }
-  console.log(total);
 }
-
-calculate(2, 5);
