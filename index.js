98152a667c01e904357988a50d8ea9958341aa94
Albus Phan <pnminh1710@gmail.com>
2018-10-19 20:27:36 +0700

    Do exercise

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+var a = [1, 2, 3, 4, 5];
+var b = [10, 20, 30];
 /**
- * Dùng vòng lặp for hiển thị ra màn hình các số từ 0 đến 9
+ * Sử dụng vòng lặp for để tính tích của các phần tử của array a với các phần tử của array b. Kết quả mong muốn:
+ * 10
+ * 20
+ * 30
+ * ...
+ * 20
+ * 40
+ * 60
+ * ...
+ * Gợi ý: Dùng nested for loops (các vòng lặp lồng nhau)
  */
 
-for (let index = 0; index < 10; index++) {
-  console.log(index);
-}
+for (let i = 0; i < a.length; i++) {
+  for (let j = 0; j < b.length; j++) {
+    console.log(a[i] * b[j]);
+  }
+  console.log('...');
+};
